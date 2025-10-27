17caa09975d67564be32f0b56de97a8a87fa16f6
Aisha Azevedo <azevedoaisha@gmail.com>
2025-10-27 18:06:19 -0300

    Update trabalho1.h

--- trabalho1/trabalho1.h
+++ trabalho1/trabalho1.h
@@ -1,1 +1,47 @@
+// #################################################
+//  Instituto Federal da Bahia
+//  Salvador - BA
+//  Curso de Análise e Desenvolvimento de Sistemas http://ads.ifba.edu.br
+//  Disciplina: INF029 - Laboratório de Programação
+//  Professor: Renato Novais - renato@ifba.edu.br
 
+//  ----- Dados do Aluno -----
+//  Nome: Aisha Azevedo Sampaio
+//  email: 20251160023@ifba.edu.br
+//  Matrícula: 20251160023
+//  Semestre: 2 semestre
+
+// #################################################
+#ifndef TRABALHO1_H
+#define TRABALHO1_H
+
+typedef struct DQ
+{
+    int iDia; 
+    int iMes;
+    int iAno;
+    int valido; // 0 se inválido, e 1 se válido 
+
+} DataQuebrada;
+
+typedef struct Qtd
+{
+    int qtdDias;
+    int qtdMeses;
+    int qtdAnos;
+    int retorno;
+
+} DiasMesesAnos;
+
+int somar(int x, int y); //função utilizada para testes
+int fatorial(int x); //função utilizada para testes
+int q1(char data[]);
+DiasMesesAnos q2(char datainicial[], char datafinal[]);
+int q3(char *texto, char c, int isCaseSensitive);
+int q4(char *strTexto, char *strBusca, int posicoes[30]);
+int q5(int num);
+int q6(int numerobase, int numerobusca);
+int q7(char matriz[8][10], char palavra[5]);
+DataQuebrada quebraData(char data[]);
+
+#endif  // MAIN_H
