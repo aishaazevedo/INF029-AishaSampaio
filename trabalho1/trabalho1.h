e0e3134d280b3488269709cf6f3d111e71c04c1a
Aisha Azevedo <azevedoaisha@gmail.com>
2025-10-29 16:42:43 -0300

    Update trabalho1.h

--- trabalho1/trabalho1.h
+++ trabalho1/trabalho1.h
@@ -15,33 +15,22 @@
 #ifndef TRABALHO1_H
 #define TRABALHO1_H
 
-typedef struct DQ
-{
-    int iDia; 
+#ifndef TRABALHO1_H
+#define TRABALHO1_H
+
+#include <stdlib.h> 
+
+typedef struct {
+    int iDia;
     int iMes;
     int iAno;
-    int valido; // 0 se inválido, e 1 se válido 
-
+    int valido; /
 } DataQuebrada;
 
-typedef struct Qtd
-{
-    int qtdDias;
-    int qtdMeses;
-    int qtdAnos;
-    int retorno;
-
-} DiasMesesAnos;
-
-int somar(int x, int y); //função utilizada para testes
-int fatorial(int x); //função utilizada para testes
-int q1(char data[]);
-DiasMesesAnos q2(char datainicial[], char datafinal[]);
-int q3(char *texto, char c, int isCaseSensitive);
-int q4(char *strTexto, char *strBusca, int posicoes[30]);
-int q5(int num);
-int q6(int numerobase, int numerobusca);
-int q7(char matriz[8][10], char palavra[5]);
+// Protótipos das funções
+int validaData(int dia, int mes, int ano);
 DataQuebrada quebraData(char data[]);
+int q1(char data[]); // Função principal 
 
-#endif  // MAIN_H
+#endif /* TRABALHO1_H */
+
