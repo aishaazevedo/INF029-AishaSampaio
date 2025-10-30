4e97fdb733dbb5f18454a4340d0c5df4fe0ba357
Aisha Azevedo <azevedoaisha@gmail.com>
2025-10-30 21:54:47 +0000

    Implementa Q2 e corrige a estrutura dos arquivos .h e .c

--- trabalho1/trabalho1.h
+++ trabalho1/trabalho1.h
@@ -1,36 +1,27 @@
-// #################################################
-//  Instituto Federal da Bahia
-//  Salvador - BA
-//  Curso de Análise e Desenvolvimento de Sistemas http://ads.ifba.edu.br
-//  Disciplina: INF029 - Laboratório de Programação
-//  Professor: Renato Novais - renato@ifba.edu.br
-
-//  ----- Dados do Aluno -----
-//  Nome: Aisha Azevedo Sampaio
-//  email: 20251160023@ifba.edu.br
-//  Matrícula: 20251160023
-//  Semestre: 2 semestre
-
-// #################################################
+// CORREÇÃO: Proteção de cabeçalho completa
 #ifndef TRABALHO1_H
 #define TRABALHO1_H
 
-#ifndef TRABALHO1_H
-#define TRABALHO1_H
-
-#include <stdlib.h> 
-
+// Estrutura necessária para a função quebraData
+// CORREÇÃO: Definição da struct (tipo) que estava faltando
 typedef struct {
     int iDia;
     int iMes;
     int iAno;
-    int valido; /
+    int valido; // Campo para indicar se a quebra foi bem-sucedida
 } DataQuebrada;
 
-// Protótipos das funções
+
+// PROTÓTIPOS DE FUNÇÕES
+// Protótipos das funções que estão em trabalho1.c
 int validaData(int dia, int mes, int ano);
 DataQuebrada quebraData(char data[]);
-int q1(char data[]); // Função principal 
+int q1(char data[]);
 
-#endif /* TRABALHO1_H */
+// Protótipos de outras funções que você usará no trabalho (exemplo, para corretor.c)
+int somar(int a, int b);
+int fatorial(int n);
+// ... adicione aqui todos os protótipos de funções globais
 
+// CORREÇÃO: Fechamento da proteção de cabeçalho
+#endif // TRABALHO1_H
