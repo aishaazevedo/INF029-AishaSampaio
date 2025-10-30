4dfd7df81ca77bc2474e3dac13af4de4f6da655a
Aisha Azevedo <azevedoaisha@gmail.com>
2025-10-30 18:46:03 -0300

    Update trabalho1.c

--- trabalho1/trabalho1.c
+++ trabalho1/trabalho1.c
@@ -12,11 +12,35 @@
 //  Semestre: 2 semestre
 // #################################################
 
-//Questão1 
+#include <stdio.h>    // Para printf/scanf (se houver) e outras funções de E/S
+#include <string.h>   // Para funções de string (strlen, etc.)
+#include <stdlib.h>   // <--- CORREÇÃO: Necessário para a função atoi()
+#include "trabalho1.h" // Seus protótipos e structs
 
-#include "trabalho1.h"
-#include <string.h>  
-#include <stdio.h>   
+// ... seu código de q1 e quebraData
+
+// Implementação da função somar (que estava faltando)
+int somar(int a, int b) {
+    // Implemente a lógica de soma aqui. Exemplo:
+    return a + b;
+}
+
+// Implementação da função fatorial (que estava faltando)
+int fatorial(int n) {
+    // Implemente a lógica de fatorial aqui. Exemplo:
+    if (n == 0)
+        return 1;
+    
+    int res = 1;
+    for (int i = 1; i <= n; i++) {
+        res *= i;
+    }
+    return res;
+}
+
+// ... e assim por diante para quaisquer outras funções que o corretor.c exija
+
+//Questão1 
 
 int validaData(int dia, int mes, int ano) {
     int quantDias;
@@ -49,77 +73,78 @@ int validaData(int dia, int mes, int ano) {
 // FUNÇÃO Quebra da String de Data (dd/mm/aa ou d/m/aaaa)
 
 DataQuebrada quebraData(char data[]){
-	DataQuebrada dq;
+    DataQuebrada dq;
     
-	char sDia[5]; 
-	char sMes[5];
-	char sAno[5];
-	int i; 
+    char sDia[5]; 
+    char sMes[5];
+    char sAno[5];
+    int i; 
     int current_index = 0; // Ponteiro para percorrer a string 'data'
     
     dq.valido = 0; 
-	
-	i = 0;
-	while (data[current_index] != '/' && data[current_index] != '\0') {
-		sDia[i] = data[current_index];	
+    
+    // --- Processa o Dia (sDia) ---
+    i = 0;
+    while (data[current_index] != '/' && data[current_index] != '\0') {
+        sDia[i] = data[current_index];  
         i++;
         current_index++;
-	}
+    }
 
-	if (i >= 1 && i <= 2) { 
-		sDia[i] = '\0';
-	} else {
+    if (i >= 1 && i <= 2) { 
+        sDia[i] = '\0';
+    } else {
         return dq; 
-	}
+    }
 
     // Pula a barra '/' e verifica se a string não acabou
     if (data[current_index] != '/') {
         return dq; 
     }
-	current_index++; 
+    current_index++; 
 
-	// --- Processa o Mês (sMes) ---
-	i = 0;
-	while (data[current_index] != '/' && data[current_index] != '\0') {
-		sMes[i] = data[current_index];
-		i++;
+    // --- Processa o Mês (sMes) ---
+    i = 0;
+    while (data[current_index] != '/' && data[current_index] != '\0') {
+        sMes[i] = data[current_index];
+        i++;
         current_index++;
-	}
+    }
 
-	if (i >= 1 && i <= 2) { 
-		sMes[i] = '\0';
-	} else {
+    if (i >= 1 && i <= 2) { 
+        sMes[i] = '\0';
+    } else {
         return dq; 
-	}
+    }
 
     if (data[current_index] != '/') {
         return dq; 
     }
-	current_index++; 
+    current_index++; 
     
 
-	// --- Processa o Ano (sAno) ---
-	i = 0;
-	while(data[current_index] != '\0') {
-	 	sAno[i] = data[current_index];
-	 	i++;
+    // --- Processa o Ano (sAno) ---
+    i = 0;
+    while(data[current_index] != '\0') {
+        sAno[i] = data[current_index];
+        i++;
         current_index++;
-	}
+    }
 
-	if(i == 2 || i == 4){ 
-		sAno[i] = '\0';
-	} else {
+    if(i == 2 || i == 4){ 
+        sAno[i] = '\0';
+    } else {
         return dq; 
-	}
+    }
 
     // Conversão das strings para inteiros 
-	dq.iDia = atoi(sDia);
-	dq.iMes = atoi(sMes);
-	dq.iAno = atoi(sAno);  
+    dq.iDia = atoi(sDia);
+    dq.iMes = atoi(sMes);
+    dq.iAno = atoi(sAno);   
 
-	dq.valido = 1; // Quebra bem-sucedida!
+    dq.valido = 1; // Quebra bem-sucedida!
     
-	return dq;
+    return dq;
 }
 // Função Principal (q1)
 
